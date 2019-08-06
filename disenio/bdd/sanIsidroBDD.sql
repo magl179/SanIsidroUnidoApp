78f23acc14d59a324346f496a76135948848ca2d
StalinMazaEpn <stalinct97@gmail.com>
2019-08-05 20:02:31 -0500

    Added created_At and updated_At fields to tables

--- disenio/bdd/sanIsidroBDD.sql
+++ disenio/bdd/sanIsidroBDD.sql
@@ -14,9 +14,11 @@ lastname VARCHAR(45) NOT NULL,
 email VARCHAR(50) NOT NULL,
 avatar VARCHAR(50) NULL,
 password VARCHAR(255) NULL,
-state TINYINT NOT NULL,
+state TINYINT NOT NULL DEFAULT 1,
 basic_service_image VARCHAR(255) NULL,
 number_phone VARCHAR(10) NULL,
+created_at datetime,
+updated_at datetime,
 CONSTRAINT pk_users PRIMARY KEY(id),
 CONSTRAINT uq_email UNIQUE(email)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
@@ -29,6 +31,8 @@ id int(255) auto_increment not null,
 user_id INT NOT NULL,
 social_id VARCHAR(255) NOT NULL,
 provider VARCHAR(255) NOT NULL,
+created_at datetime,
+updated_at datetime,
   CONSTRAINT pk_social_profiles PRIMARY KEY(id),
   CONSTRAINT fk_social_profiles_user FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
@@ -41,8 +45,10 @@ id int(255) auto_increment not null,
 device_id VARCHAR(255) NOT NULL,
 description TEXT NULL,
 user_id INT NOT NULL,
-  CONSTRAINT pk_devices PRIMARY KEY(id),
-  CONSTRAINT fk_devices_user FOREIGN KEY(user_id) REFERENCES users(id)
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_devices PRIMARY KEY(id),
+CONSTRAINT fk_devices_user FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 -- -----------------------------------------------------
@@ -53,8 +59,10 @@ id int(255) auto_increment not null,
 name VARCHAR(45) NOT NULL,
 description TEXT NOT NULL,
 ubication JSON NULL,
-  CONSTRAINT pk_public_services PRIMARY KEY(id)
-  )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_public_services PRIMARY KEY(id)
+)ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 -- -----------------------------------------------------
 -- Table phones
@@ -63,8 +71,10 @@ CREATE TABLE IF NOT EXISTS phones (
 id int(255) auto_increment not null,
 public_service_id INT NOT NULL,
 phone_number VARCHAR(10) NOT NULL,
-   CONSTRAINT pk_phones PRIMARY KEY(id),
-   CONSTRAINT fk_phone_public_service FOREIGN KEY(public_service_id) REFERENCES public_services(id)
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_phones PRIMARY KEY(id),
+CONSTRAINT fk_phone_public_service FOREIGN KEY(public_service_id) REFERENCES public_services(id)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 -- -----------------------------------------------------
@@ -75,8 +85,10 @@ id int(255) auto_increment not null,
 name VARCHAR(45) NOT NULL,
 slug VARCHAR(45) NOT NULL,
 description TEXT(150) NULL,
-  CONSTRAINT pk_categories PRIMARY KEY(id)
-  )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_categories PRIMARY KEY(id)
+)ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 
 -- -----------------------------------------------------
@@ -91,10 +103,12 @@ time TIME NOT NULL,
 ubication JSON NULL,
 user_id INT NOT NULL,
 category_id INT NOT NULL,
-  CONSTRAINT pk_posts PRIMARY KEY(id),
-  CONSTRAINT fk_posts_users FOREIGN KEY(user_id) REFERENCES users(id),
-  CONSTRAINT fk_posts_categories FOREIGN KEY(category_id) REFERENCES categories(id)
-  )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_posts PRIMARY KEY(id),
+CONSTRAINT fk_posts_users FOREIGN KEY(user_id) REFERENCES users(id),
+CONSTRAINT fk_posts_categories FOREIGN KEY(category_id) REFERENCES categories(id)
+)ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 
 -- -----------------------------------------------------
@@ -104,8 +118,10 @@ CREATE TABLE IF NOT EXISTS images (
 id int(255) auto_increment not null,
 url VARCHAR(256) NOT NULL,
 post_id INT NOT NULL,
-  CONSTRAINT pk_imagenes PRIMARY KEY(id),
- CONSTRAINT fk_imagenes_posts FOREIGN KEY(post_id) REFERENCES posts(id)
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_imagenes PRIMARY KEY(id),
+CONSTRAINT fk_imagenes_posts FOREIGN KEY(post_id) REFERENCES posts(id)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 
@@ -118,8 +134,10 @@ name VARCHAR(45) NOT NULL,
 slug VARCHAR(45) NOT NULL,
 description TEXT NULL,
 category_id INT NOT NULL,
-  CONSTRAINT pk_subcategory PRIMARY KEY(id),
-  CONSTRAINT fk_subcategory_of_category FOREIGN KEY(category_id) REFERENCES categories(id)
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_subcategory PRIMARY KEY(id),
+CONSTRAINT fk_subcategory_of_category FOREIGN KEY(category_id) REFERENCES categories(id)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 
@@ -131,6 +149,8 @@ id int(255) auto_increment not null,
 name VARCHAR(255) NOT NULL,
 slug VARCHAR(255) NOT NULL,
 description TEXT NULL,
+created_at datetime,
+updated_at datetime,
 CONSTRAINT pk_roles PRIMARY KEY(id)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
@@ -143,8 +163,10 @@ id int(255) auto_increment not null,
 name VARCHAR(255) NOT NULL,
 slug VARCHAR(255) NOT NULL,
 description TEXT NULL,
-  CONSTRAINT pk_permissions PRIMARY KEY(id)
-  )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
+created_at datetime,
+updated_at datetime,
+CONSTRAINT pk_permissions PRIMARY KEY(id)
+)ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 
 -- -----------------------------------------------------
@@ -155,6 +177,8 @@ id int(255) auto_increment not null,
 user_id INT NOT NULL,
 role_id INT NOT NULL,
 position VARCHAR(10) NULL,
+created_at datetime,
+updated_at datetime,
 CONSTRAINT pk_role_user PRIMARY KEY(id),
 CONSTRAINT fk_roles_to_user FOREIGN KEY(role_id) REFERENCES roles(id),
 CONSTRAINT fk_users_from_role FOREIGN KEY(user_id) REFERENCES users(id)
@@ -168,9 +192,11 @@ CREATE TABLE IF NOT EXISTS permission_user (
 id int(255) auto_increment not null,
 user_id INT NOT NULL,
 permission_id INT NOT NULL,
+created_at datetime,
+updated_at datetime,
 CONSTRAINT pk_permission_user PRIMARY KEY(id),
-  CONSTRAINT fk_users_has_permissions_permissions FOREIGN KEY(permission_id) REFERENCES permissions(id),
-  CONSTRAINT fk_users_has_permissions_users FOREIGN KEY(user_id) REFERENCES users(id)
+CONSTRAINT fk_users_has_permissions_permissions FOREIGN KEY(permission_id) REFERENCES permissions(id),
+CONSTRAINT fk_users_has_permissions_users FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDb DEFAULT CHARSET=utf8mb4;
 
 
@@ -181,6 +207,8 @@ CREATE TABLE IF NOT EXISTS permission_role (
 id int(255) auto_increment not null,
 role_id INT NOT NULL,
 permission_id INT NOT NULL,
+created_at datetime,
+updated_at datetime,
 CONSTRAINT pk_permission_role PRIMARY KEY(id),
 CONSTRAINT fk_roles_has_permissions_permissions FOREIGN KEY(permission_id) REFERENCES permissions(id),
 CONSTRAINT fk_roles_has_permissions_roles FOREIGN KEY(role_id) REFERENCES roles(id)
@@ -199,6 +227,8 @@ time TIME NULL,
 like_count TINYINT NULL,
 attendance TINYINT NULL,
 nonattendance TINYINT NULL,
+created_at datetime,
+updated_at datetime,
 CONSTRAINT pk_details PRIMARY KEY(id),
 CONSTRAINT fk_details_posts FOREIGN KEY(post_id) REFERENCES posts(id),
 CONSTRAINT fk_details_users FOREIGN KEY(user_id) REFERENCES users(id),
