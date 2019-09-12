32314802922b6ce078d899fd2a495a975038ea60
StalinMazaEpn <stalinct97@gmail.com>
2019-09-11 22:38:32 -0500

    Redisenio Preguntas Frecuentes y Login y Registro en proceso de disenio

--- code/src/app/pages/frequent-questions/frequent-questions.page.ts
+++ code/src/app/pages/frequent-questions/frequent-questions.page.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrequentQuestionsPage implements OnInit {
 
+    automaticClose = true;
+    frequentQuestions = [
+        {
+            title: 'Redes Sociales Asociadas',
+            description: 'Las Redes Sociales Asociadas te permiten que puedas iniciar sesión por medio de tu cuenta de Facebook o Google, si deseas desconectar una cuenta social asegurate primero de tener una contraseña activa, caso contrario cambia tu contraseña para que puedas iniciar sesión por medio de un formulario',
+            icon: 'help-circle-outline',
+            open: true
+        },
+        {
+            title: 'Dispositivos Asociados',
+            description: ' Los dispositivos asociados se registran cuando inicias sesión para permitir que recibas notificaciones en tu celular, si desconectas un dispositivo ya no podrás recibir notificaciones en dicho dispositivo',
+            icon: 'help-circle-outline',
+            open: false
+        },
+        {
+            title: 'No puedo visualizar las opciones en la aplicación',
+            description: 'Si observas algún problema con la aplicación, cierra y vuelve a abrir la aplicación o cierra sesión y vuelve a iniciar sesión para comprobar si el problema persiste',
+            icon: 'git-compare',
+            open: false
+        }
+    ];
+
   constructor() { }
 
   ngOnInit() {
   }
+    
+  toggleSection(index, hasChild) {
+    this.frequentQuestions[index].open = !this.frequentQuestions[index].open;
+    if (this.automaticClose && this.frequentQuestions[index].open) {
+        this.frequentQuestions
+            .filter((item, itemIndex) => {
+                if (hasChild) {
+                    return itemIndex !== index;
+                }
+            })
+            .map(item => item.open = false);
+    }
+}
 
 }
--- test/logs/errors_log.md
+++ test/logs/errors_log.md
@@ -18,7 +18,7 @@ cordova.system.library.5=com.google.android.gms:play-services-identity:11.8.0
 
 El Problema es que el plugin de Geolocalización no puede detectar o ejecutar la función de obtener coordenadas si el GPS o los servicios de localización están desactivados, por lo que primero se debe verificar si se tiene permisos de localización, luego pedir activar el GPS y luego si obtener la localización
 
-## Error ERR_CLEARTEXT_NOT_PERMITTED 
+## Error ERR_CLEARTEXT_NOT_PERMITTED o cuando no hace peticion es por el network security fole
 
 ```bash
 <access origin="*" />
