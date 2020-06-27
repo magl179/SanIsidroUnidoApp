dd5904aba1255d9e706f884d8e323b3502f65a87
StalinMazaEPN <stalinct97@gmail.com>
2020-06-27 18:45:37 -0500

    mejoras en el listado de notificaciones

--- src/app/interfaces/models.ts
+++ src/app/interfaces/models.ts
@@ -301,6 +301,7 @@ export interface INotificationApi {
 export interface INotiList {
     title?: string;
     message?: string;
+    description?: string;
     neighbor?: INotificationUser;
     notification_user?: INotificationUser;
     post?: IPost;
